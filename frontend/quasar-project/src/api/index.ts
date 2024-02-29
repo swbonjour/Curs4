f145f81fea12137f41874f1bb70e051cd0642fe1
swbonjour <71038541+swbonjour@users.noreply.github.com>
2024-02-29 11:15:01 +0300

    auth

--- frontend/quasar-project/src/api/index.ts
+++ frontend/quasar-project/src/api/index.ts
@@ -1,9 +1,10 @@
-import { LangApi } from "./client";
+import { LangApi } from './client';
 
 export const client = new LangApi({
-    secure: true,
-    baseURL: 'http://localhost:3000',
-    securityWorker: accessToken => accessToken ? { headers: { Authorization: `Bearer ${accessToken}`}} : {}
-})
+  secure: true,
+  baseURL: 'http://localhost:3000',
+  securityWorker: (accessToken) =>
+    accessToken ? { headers: { Authorization: `Bearer ${accessToken}` } } : {},
+});
 
 export const ApiClient = client.api;
